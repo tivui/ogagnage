5734729454f5700e103fa18f853d52ddf84112f6
tivui <tivui64@gmail.com>
2020-12-15 01:56:03 +0100

    Changement de l'URI DB (suppression de la variable d'environnement et copié colé de l'url d'heroku)

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,10 @@ db = SQLAlchemy(app)
 
 app.config["UPLOAD_FOLDER"] = "C:/Users/Asus/Documents/Formations/Python/ogagnage/app/static/img/"
 app.config["SECRET_KEY"] = "Perlembourg49%%%"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/ecnelisflydb.db"
-if app.config["ENV"]=='prod' and not KeyError():
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+if app.config["ENV"]=='prod':
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://yfbpfzchmeimka:c2a276287cf7cab71b0d92600f28f2104d449a3fd061d50a6f73f41830250820@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dnuo27dukd0qh"
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:Perlembourg49%%%@localhost/ogagnagedb'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 class User_table(db.Model):
