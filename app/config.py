e15d5ae3476c924b81bec640355253d1b4e21387
tivui <tivui64@gmail.com>
2020-12-15 05:33:46 +0100

    changement mots de passe S3

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
 S3_LOCATION = 'http://your-amazon-site.amazonaws.com/'
-S3_KEY = "AKIAJGPPZK6VK7JXIPZA"
-S3_SECRET = "Gor67Wox0uSdo2j40W2MCVqjzzDPdUAfbeEaexk/"
+S3_KEY = "AKIAIVGQWMSGBWJKWOUA"
+S3_SECRET = "wjSaMDFhiuJ1jNrMnGQmoFYCvV0/C6GFWOkOUVu0"
 S3_BUCKET = "ogagnage"
 DATABASE_URL = "postgres://fyuujerfzmpxgf:76e9998da230d3998b8cb4f145fbe10d326c77f252b47e5fbabb188f8aeb0f9e@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/dchmdui7vcgm07"
 SQLALCHEMY_DATABASE_URI = "postgres://fyuujerfzmpxgf:76e9998da230d3998b8cb4f145fbe10d326c77f252b47e5fbabb188f8aeb0f9e@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/dchmdui7vcgm07"
