31a7a521d2ee90b66188331860970d7a4c94551e
tivui <tivui64@gmail.com>
2020-12-17 15:47:52 +0100

    Nouvelle tentative de résolution du bug SQL Alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,7 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config["UPLOAD_FOLDER"] = "C:/Users/Asus/Documents/Formations/Python/ogagnage/app/static/img/"
 app.config["SECRET_KEY"] = "Perlembourg49%%%"
-if app.config["ENV"]=='prod':
-    DATABASE_URL = "postgres://fyuujerfzmpxgf:76e9998da230d3998b8cb4f145fbe10d326c77f252b47e5fbabb188f8aeb0f9e@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/dchmdui7vcgm07"
-    SQLALCHEMY_DATABASE_URI = "postgres://fyuujerfzmpxgf:76e9998da230d3998b8cb4f145fbe10d326c77f252b47e5fbabb188f8aeb0f9e@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/dchmdui7vcgm07"
-else:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Perlembourg49%%%@localhost/ogagnagedb'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 class User_table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
